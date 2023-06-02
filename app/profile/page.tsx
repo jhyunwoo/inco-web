c886241d396261b7972258897f570cea7803575d
Hyunwoo <88994338+jhyunwoo@users.noreply.github.com>
2023-06-02 15:59:09 +0900

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -62,7 +62,7 @@ export default async function Profile() {
             </div>
           </div>
         </div>
-        <div className="bg-white p-4 rounded-lg shadow-xl grid grid-cols-2 gap-2">
+        <div className="bg-white p-4 rounded-lg shadow-xl grid grid-cols-1 gap-2">
           <div className="col-span-2 text-lg font-semibold text-center">
             랭크 기준
           </div>
