7945bc89a975ad80d6ac83907b0610828d353f32
jhyunwoo <jhyunwoo0228@gmail.com>
2023-06-02 22:32:29 +0900

    add kakao login

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 // import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import KakaoProvider from "next-auth/providers/kakao";
 import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
@@ -11,9 +12,13 @@ export const authOptions: NextAuthOptions = {
     //   clientId: process.env.GITHUB_ID!,
     //   clientSecret: process.env.GITHUB_SECRET!,
     // }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID!,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    // }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
     }),
   ],
 };
